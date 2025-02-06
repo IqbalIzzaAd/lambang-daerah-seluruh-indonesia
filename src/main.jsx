ceb219491dd48d89b34cd055546fe6fff8c5ceb2
IqbalIzzaAd <iqbal@metabharata.com>
2025-02-06 09:14:55 +0700

    update4

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import LoadingPage from "./pages/LoadingPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/index.css";
 
 
