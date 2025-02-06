623094ec0c3217b4de3fc41dc2773d0509861e74
IqbalIzzaAd <iqbal@metabharata.com>
2025-02-06 09:05:33 +0700

    update3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import LoadingPage from "./pages/LoadingPage";
-import ScrollToTop from "./components/ScrollToTop";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/index.css";
 
 
@@ -24,10 +21,7 @@ const Root = () => {
       {isLoading ? (
         <LoadingPage />
       ) : (
-        <BrowserRouter>
-          <ScrollToTop />
           <App />
-        </BrowserRouter>
       )}
     </React.StrictMode>
   );
